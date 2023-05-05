0474511d3960ac7693422d284c17f9eab1b02de0
Nicolas723 <nbarn@misena.edu.co>
2023-05-05 00:03:24 -0500

    first commit

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,122 +3,114 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.ComposeTutorial.ui.theme.ComposeTutorial
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Surface
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 import androidx.compose.foundation.border
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.foundation.clickable
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme() {
+            ComposeTutorialTheme {
                 Conversation(SampleData.conversationSample)
             }
         }
     }
 }
-
-@Composable
-fun MessageCard(name:String){
-    Text("Hello $name!")
-}
-/*
-@Preview(showBackground = true)
-@Composable
-fun MessageCardPreview(){
- MessageCard(name = "Android")
-*/
-
-
 data class Message(val author: String, val body: String)
+
 @Composable
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(R.drawable.luna),
-            contentDescription = "Contact profile picture",
+            painter = painterResource(R.drawable.gato),
+            contentDescription = null,
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp,
-                    MaterialTheme.colorScheme.secondary,
-                    CircleShape
-                )
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
         )
-
         Spacer(modifier = Modifier.width(8.dp))
 
+        // We keep track if the message is expanded or not in this
+        // variable
         var isExpanded by remember { mutableStateOf(false) }
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-
-        Text(
-            text = msg.author,
-            color = MaterialTheme.colors.secondaryVariant,
-            style = MaterialTheme.typography.subtitle2
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
         )
 
-        Spacer(modifier = Modifier.height(8.dp))
-        Surface(
-            shape = MaterialTheme.shapes.medium,
-            ShadowElevation = 1.dp,
-        ) {
-            Text(text = msg.body,
-                modifier = Modifier.padding(all = 4.dp),
-                // If the message is expanded, we display all its content
-                // otherwise we only display the first line
-                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                style = MaterialTheme.typography.body2)
-        } }
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
     }
 }
+
 
 
 
 @Preview
 @Composable
-fun PreviewMessageCard() { 
-    ComposeTutorialTheme(){
-        Surface { 
+fun PreviewMessageCard() {
+    ComposeTutorialTheme {
+        Surface {
             MessageCard(
-                msg = Message("Santiago", "Take a look at Jetpack Compose, it's great!")
-            ) 
-        } 
+                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
     }
 }
-
 
 @Composable
 fun Conversation(messages: List<Message>) {
@@ -129,12 +121,12 @@ fun Conversation(messages: List<Message>) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun PreviewConversation() {
-    ComposeTutorialTheme() {
-         Conversation(SampleData.conversationSample)
-     }
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
 
 
